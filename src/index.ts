f14640e819d4c7c9cbcfa7712ab9f53dec0cf019
Felix Kaminski <kaminski.felix@gmail.com>
2026-04-14 13:51:44 +0200

    fix: exposes missing types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+export type { ISentryFastifyErrorHandlerOpts, SentryFastifyEnrichScope } from './sentry-fastify-plugin';
+
 export { sentryFastifyErrorHandlerPlugin as default } from './sentry-fastify-plugin';
